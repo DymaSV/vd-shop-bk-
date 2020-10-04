412456b1e4025de23c711a04255075481c31e689
Dmytro Sv <dymasvyrydonov@gmail.com>
2020-10-03 22:45:20 -0400

    First commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b0ad04492a685528f46287e69cc127de'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a21cbd4b-3d01-4103-9101-83c3f124416b'
+};
